fb5d944b1a222ec8562a33624880a979c246f3c5
Rodrigo Rosas <rodrigo.rosasv@gmail.com>
2020-08-24 20:49:42 -0500

    Updates

---
+++ precache-manifest.2f5ca2bc293b9a1f2bd089403c340061.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89eb439df61439ff2f143c90af38def5",
+    "url": "/rrv-react-portfolio/index.html"
+  },
+  {
+    "revision": "419e597539de3d076cf7",
+    "url": "/rrv-react-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a06e92da9c90721c630e",
+    "url": "/rrv-react-portfolio/static/js/2.c6c8ef31.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rrv-react-portfolio/static/js/2.c6c8ef31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "419e597539de3d076cf7",
+    "url": "/rrv-react-portfolio/static/js/main.6b376776.chunk.js"
+  },
+  {
+    "revision": "53a88fafa08195d5eff9",
+    "url": "/rrv-react-portfolio/static/js/runtime-main.5236fad2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rrv-react-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
